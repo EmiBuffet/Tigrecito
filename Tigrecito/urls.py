271cade0a38eac77874140bf3e8e8e3c957def16
EmilianoBuffet <emibuffet@gmail.com>
2018-11-27 16:23:33 -0300

    lpm

--- Tigrecito/urls.py
+++ Tigrecito/urls.py
@@ -34,6 +34,6 @@ urlpatterns = [
     path('api/clubes', views.CategoriasClub, name="api_clubes"),
     path('partido/equipo/<str:clubName>/<str:clubId>/<str:categoryId>', views.partidosEquipo)
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
-                   static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+               static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
