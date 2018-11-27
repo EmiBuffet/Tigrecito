917bbf5e8c553d6458db0f8be383cbe1fdcfac45
EmilianoBuffet <emibuffet@gmail.com>
2018-11-27 15:58:44 -0300

    Update ALLOWED_HOSTS with site and development server URL

--- Tigrecito/settings.py
+++ Tigrecito/settings.py
@@ -23,13 +23,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = 'vur(1!*$oc)09=3g@!=8_l%z1d@&2$*fkfbr2*=bry#gd1)x)y'
 import os
 
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'vur(1!*$oc)09=3g@!=8_l%z1d@&2$*fkfbr2*=bry#gd1)x)y')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['tigrecitosunchales.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
