23120946aa41375bc58f31b7ab032fb545f73fa2
EmilianoBuffet <emibuffet@gmail.com>
2018-11-27 17:24:18 -0300

    aaa

--- staticfiles/js/globalConfig.js
+++ staticfiles/js/globalConfig.js
@@ -1,10 +1,10 @@
 'use strict';
 var HOST_DEV = 'http://127.0.0.1:8000'
-var HOST_PRODUC = 'http://tigrecitosunchales.com'
+var HOST_PRODUC = 'http://tigrecitosunchales.herokuapp.com'
 var HOST;
 
 // poner en false si se pone en producción.
-const DEV_MODE = true
+const DEV_MODE = false
 
 if(DEV_MODE) {
 	HOST = HOST_DEV
