28875fe8710d6e4c9edb2274df4d7a467d9e2bc5
EmilianoBuffet <emibuffet@gmail.com>
2018-11-29 07:25:37 -0300

    Porbando Algo

--- static/js/globalConfig.js
+++ static/js/globalConfig.js
@@ -1,10 +1,10 @@
 'use strict';
 var HOST_DEV = 'http://127.0.0.1:8000'
-var HOST_PRODUC = 'https://www.tigrecitosunchales.com'
+var HOST_PRODUC = 'https://tigrecitosunchales.herokuapp.com'
 var HOST;
 
 // poner en false si se pone en producción.
-const DEV_MODE = false
+const DEV_MODE = true
 
 if(DEV_MODE) {
 	HOST = HOST_DEV
