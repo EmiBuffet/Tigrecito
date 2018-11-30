14352bb8888d12371fafd250d0ffef2ef11f0b0e
lucasdavidferrero <lucasdavidferrero@gmail.com>
2018-11-30 13:54:49 -0300

    escribo funcion para subir imagen

--- static/js/cambiarEscudo.js
+++ static/js/cambiarEscudo.js
@@ -6,7 +6,13 @@ var vmEscudos = new Vue({
     data: {
         clubs: [],
         isSearching: true,
-        error: null
+        error: null,
+        selectedFile: null,
+        upload: {
+            isUploading: false,
+            percentage: 0,
+            isUploadingToDjango: false
+        }
     },
     methods: {
         fetchAllClubs: function() {
@@ -25,6 +31,35 @@ var vmEscudos = new Vue({
         },
         onCloseAlertError: function() {
             this.error = null
+        },
+        onUploadImageToFirebase: function(equipo, event) {
+            this.upload.percentage = 0
+            this.isUploadingToDjango = false
+
+            //upload image to firebase
+            let storageRef = firebase.storage().ref(`shield/${equipo.id}/${this.selectedFile.name}`)
+            console.log(storageRef.fullPath)
+            var uploadTask = storageRef.put(this.selectedFile)
+            this.upload.isUploading = true
+            //update progress
+            uploadTask.on(firebase.storage.TaskEvent.STATE_CHANGED,
+                function(snapshot) {
+                    this.upload.percentage = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+                    console.log('va: ' + this.upload.percentage + '%')
+                },
+                function(err) {
+                    this.error = err
+                    this.upload.isUploading = false
+                },
+                function() {
+                    this.upload.isUploading = false
+                }
+            )
+            //upload to django server
+        },
+        onChangeInputFile: function(event) {
+            this.selectedFile = event.target.files[0]
+            console.log(this.selectedFile)
         }
     }
 })
